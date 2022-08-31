8cb4c3bef5457c79c14f5371bd5c647a47b7e509
gcnyin <guchounongyin@gmail.com>
2022-08-31 23:40:34 +0800

    [feat] Updated App2

--- src/main/scala/example/App2.scala
+++ src/main/scala/example/App2.scala
@@ -8,16 +8,14 @@ object App2 extends App {
 
   final case class ChildA(name: String, age: Int) extends Parent
   object ChildA {
-    implicit val writes: Writes[ChildA] =
-      ((JsPath \ "name").write[String] and (JsPath \ "age").write[Int])(unlift(ChildA.unapply))
-    implicit val reads: Reads[ChildA] =
-      ((JsPath \ "name").read[String] and (JsPath \ "age").read[Int])(ChildA.apply _)
+    implicit val format: Format[ChildA] =
+      ((__ \ "name").format[String] and (__ \ "age").format[Int])(ChildA.apply, unlift(ChildA.unapply))
   }
 
   final case class ChildB(amount: Double) extends Parent
   object ChildB {
-    implicit val writes: Writes[ChildB] = (JsPath \ "amount").write[Double].contramap(c => c.amount)
-    implicit val reads: Reads[ChildB] = (JsPath \ "amount").read[Double].map(ChildB(_))
+    implicit val writes: Format[ChildB] =
+      (__ \ "amount").format[Double].bimap(ChildB.apply, unlift(ChildB.unapply))
   }
 
   println(Json.toJson(ChildA("tom", 23))) // {"name":"tom","age":23}
